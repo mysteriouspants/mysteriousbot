f5abafe5cbc35414a3e73fc3c1546a3441879d92
Christopher R. Miller <xpm@mysteriouspants.com>
2020-07-18 16:13:55 -0700

    Use rsync to make deployment faster

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ cargo build --release
 if [ $? -eq 0 ]; then
   ssh ${SERVICE_SU_USER}@${SERVICE_HOST} sudo systemctl stop ${SERVICE_NAME}
   ssh ${SERVICE_USER_NAME}@${SERVICE_HOST} mkdir -p ${SERVICE_USER_DIR}/config
-  scp config/mysteriousbot.toml ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/config/mysteriousbot.toml
-  scp target/release/mysteriousbot ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/mysteriousbot
+  rsync -avzr config/mysteriousbot.toml ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/config/mysteriousbot.toml
+  rsync -avzr target/release/mysteriousbot ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/mysteriousbot
   ssh ${SERVICE_USER_NAME}@${SERVICE_HOST} chmod +x ${SERVICE_USER_DIR}/mysteriousbot
   ssh ${SERVICE_SU_USER}@${SERVICE_HOST} sudo systemctl start ${SERVICE_NAME}
 else
