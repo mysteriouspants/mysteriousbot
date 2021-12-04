6cc9af028304ff615fb6ccf9396cf2691d90ae11
Christopher R. Miller <xpm@mysteriouspants.com>
2021-12-03 19:24:36 -0800

    Revert "Remove mysterious_cache"
    This reverts commit c7a0ceb3ede9d71de7c6e2ac250a2575320cff4b.

--- src/emojicache.rs
+++ src/emojicache.rs
@@ -1,9 +1,8 @@
-use caches::{Cache, AdaptiveCache};
-use std::{time::{Duration, Instant}, sync::Arc};
+use std::time::Duration;
 use thiserror::Error;
+use mysterious_cache::{ExpiringCache, SharedCache};
 use serenity::{client::Context, model::{guild::Emoji, id::GuildId}};
 use serenity::Error as DiscordError;
-use parking_lot::RwLock;
 
 #[derive(Debug, Error)]
 pub enum GetEmojiError {
@@ -11,24 +10,19 @@ pub enum GetEmojiError {
   Discord(#[from] DiscordError),
 }
 
-struct CacheValue {
-    emojis: Vec<Emoji>,
-    inserted_at: Instant,
-}
-
 /// An LRU Cache which holds the emojis - cached because hitting the
 /// Discord emoji API potentially on every event sounds like a bad time.
 pub struct EmojiCache {
-    cache: Arc<RwLock<AdaptiveCache<GuildId, CacheValue>>>,
-    timeout: Duration,
+    cache: SharedCache<ExpiringCache<GuildId, Vec<Emoji>>, GuildId, Vec<Emoji>>,
 }
 
 impl EmojiCache {
     pub fn new() -> Self {
-        let cache: AdaptiveCache<GuildId, CacheValue> = AdaptiveCache::new(5).unwrap();
         Self {
-            cache: Arc::from(RwLock::from(cache)),
-            timeout: Duration::from_secs(24 * 60 * 60),
+            cache: SharedCache::with_cache(ExpiringCache::with_capacity_and_timeout(
+                5,
+                Duration::from_secs(24 * 60 * 60),
+            )),
         }
     }
     pub async fn get_emoji(&self, ctx: &Context, guild_id: &GuildId, twemoji: &str) -> Result<Option<Emoji>, GetEmojiError> {
@@ -37,23 +31,13 @@ impl EmojiCache {
         emoji.name == twemoji
       }).next().map(Emoji::clone))
     }
-
     async fn get_emojis(&self, ctx: &Context, guild_id: &GuildId) -> Result<Vec<Emoji>, GetEmojiError> {
-        // look up stored emojis
-        if let Some(value) = self.cache.write().get(guild_id) {
-            if value.inserted_at.elapsed() > self.timeout {
-                return Ok(value.emojis.clone());
-            }
-        }
-
-        // try to load new emojis
+      if let Some(emojis) = self.cache.get(guild_id) {
+        return Ok(emojis);
+      } else {
         let emojis = guild_id.emojis(ctx).await?;
-
-        self.cache.write().put(*guild_id, CacheValue {
-            emojis: emojis.clone(),
-            inserted_at: Instant::now(),
-        });
-
-        Ok(emojis)
+        self.cache.insert(*guild_id, emojis.clone());
+        return Ok(emojis);
+      }
     }
 }
